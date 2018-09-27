b44c1e277c531a77b70dd4ee2546e0448998b5c3
Luke Snowden <mail@lukesnowden.co.uk>
2018-09-27 16:21:05 +0100

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->app->booted( function( $app ) {
-            $app[ RegisterBlueprint::class ]->attachView( new CustomerEditView );
+            //$app[ RegisterBlueprint::class ]->attachView( new CustomerEditView );
         });
     }
 
